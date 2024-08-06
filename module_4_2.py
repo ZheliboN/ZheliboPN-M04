b898b5482a98118faa9a4636f3ac282e1067a00e
ZheliboN <zhelibo_pavel@mail.ru>
2024-08-06 08:17:17 +0800

    Add files via upload
    Домашняя работа по уроку "Пространство имен."

---
+++ module_4_2.py
@@ -0,0 +1,9 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+
+test_function()
+inner_function()
+# inner_function() - внутри функции test_function, вызов напраямую из прогррамы невозможен
